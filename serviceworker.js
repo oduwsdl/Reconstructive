c1fe62fc1631ffbb0080c925ac214aedcb80f77b
Sawood Alam <ibnesayeed@gmail.com>
2017-12-18 10:23:27 -0500

    Minor changes to make it play nice when hosted on GitHub Pages

--- serviceworker.js
+++ serviceworker.js
@@ -2,7 +2,9 @@ console.log('ServiceWorker downloaded');
 
 importScripts('reconstructive.js');
 
-reconstructive.init({urimPattern: self.location.origin + '/archived/<datetime>/<urir>'});
+reconstructive.init({
+  urimPattern: self.location.href.substring(0, self.location.href.lastIndexOf('/')) + '/archived/<datetime>/<urir>'
+});
 
 self.addEventListener("install", function(event) {
   console.log('ServiceWorker installed');
