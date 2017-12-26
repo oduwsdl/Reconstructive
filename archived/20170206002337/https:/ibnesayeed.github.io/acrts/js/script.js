d1601e613447db2045396295d3a73b85625d53cd
Sawood Alam <ibnesayeed@gmail.com>
2017-12-26 13:02:19 -0500

    Integrated ACRTS, closes #2

---
+++ archived/20170206002337/https:/ibnesayeed.github.io/acrts/js/script.js
@@ -0,0 +1,3 @@
+document.querySelector('#js-file .result').style.backgroundColor = '#0f0';
+var loc = 'https://ibnesayeed.github.io/acrts/img/by-js-file.png'
+document.querySelector('#by-js-file .result').style.backgroundImage = "url('" + loc + "')";
