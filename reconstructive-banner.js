82f121582e864a1d55e428235ab9a710147a4c4b
Sawood Alam <ibnesayeed@gmail.com>
2018-07-29 23:52:15 -0400

    Implement multi-state banner

--- reconstructive-banner.js
+++ reconstructive-banner.js
@@ -1,76 +1,231 @@
+/**
+ * [ReconstructiveBanner](https://oduwsdl.github.io/Reconstructive/reconstructive-banner.js) implements `<reconstructive-banner>` [Custom Element](https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements).
+ * It is an unobtrusive archival replay banner to make [mementos](http://mementoweb.org/about/) interactive and surface on-demand metadata about the archived resource.
+ *
+ * @overview  ReconstructiveBanner implements <reconstructive-banner> Custom Element for archival replay banners.
+ * @author    Sawood Alam <ibnesayeed@gmail.com>
+ * @license   MIT
+ * @copyright ODU Web Science / Digital Libraries Research Group 2017
+ */
 class ReconstructiveBanner extends HTMLElement {
+  /**
+   * Creates a new ReconstructiveBanner instance and attaches a Shadow DOM.
+   */
   constructor() {
     super();
     this.shadow = this.attachShadow({mode: 'closed'});
   }
 
   connectedCallback() {
-    this.urir = this.getAttribute('urir');
-    this.datetime = this.getAttribute('datetime');
-    console.log(this.urir);
-    let template = `
+    this.logoSrc = this.getAttribute('logo-src') || 'data:image/svg+xml;charset=utf-8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+PHBhdGggZD0iTTAgMyBhMyAzIDAgMCAxIDMtMyBoMiBsMyAzIGgtMyBhMiAyIDAgMCAwLTIgMiB2NiBhMiAyIDAgMCAwIDIgMiBoMSBsMyAzIGgtNiBhMyAzIDAgMCAxLTMtMyBaIiBmaWxsPSIjMUI0ODY5IiAvPjxwYXRoIGQ9Ik0xNiAxNiBoLTQgbC05LTkgaDYgYTIgMiAwIDAgMCAwLTQgaC0xIGwtMy0zIGg2IGEzIDMgMCAwIDEgMyAzIHY0IGEzIDMgMCAwIDEtMyAzIGgtMSBaIiBmaWxsPSIjRjI0NzM4IiAvPjwvc3ZnPg==';
+    this.urir = this.getAttribute('urir') || '';
+    this.mementoDatetime = this.getAttribute('memento-datetime') || '';
+    this.firstUrim = this.getAttribute('first-urim') || '';
+    this.firstDatetime = this.getAttribute('first-datetime') || '';
+    this.lastUrim = this.getAttribute('last-urim') || '';
+    this.lastDatetime = this.getAttribute('last-datetime') || '';
+    this.prevUrim = this.getAttribute('prev-urim') || '';
+    this.prevDatetime = this.getAttribute('prev-datetime') || '';
+    this.nextUrim = this.getAttribute('next-urim') || '';
+    this.nextDatetime = this.getAttribute('next-datetime') || '';
+
+    this.timeDiff = (() => {
+      const diff = Date.now() - new Date(this.mementoDatetime);
+      if (isNaN(diff)) {
+        return '';
+      }
+      if (diff < 0) {
+        return 'Capture from the future!';
+      }
+      if (diff >= 31622400000) {
+        return `Captured ${Math.round(diff / 31622400000)} year(s) ago`;
+      }
+      if (diff >= 2629746000) {
+        return `Captured ${Math.round(diff / 2629746000)} month(s) ago`;
+      }
+      if (diff >= 86400000) {
+        return `Captured ${Math.round(diff / 86400000)} day(s) ago`;
+      }
+      if (diff >= 3600000) {
+        return `Captured ${Math.round(diff / 3600000)} hour(s) ago`;
+      }
+      return `Captured ${Math.round(diff / 60000)} minute(s) ago`;
+    })();
+
+    const template = `
       <style>
-        div#reconstructive-banner-aligner {
-          width: 100%;
-          position: fixed;
-          bottom: 0;
-          transition: opacity 0.5s ease-in;
+        a[href=''] {
+          pointer-events: none;
         }
-        div.hidden {
+        #wrapper {
+          z-index: 99999999;
+          padding: 10px;
+          box-sizing: border-box;
+        }
+        #wrapper.fab {
+          position: fixed;
+          bottom: 10px;
+          right: 10px;
+          transition: all 0.5s ease-in;
+        }
+        #wrapper.expanded {
+          position: fixed;
+          top: 0;
+          left: 0;
+          margin: 0;
+          width: 100%;
+          height: 100%;
+          background: rgba(100, 100, 100, 0.6);
+          transition: all 0.5s ease-out;
+        }
+        #wrapper.hidden {
           opacity: 0;
           transition: opacity 0.5s ease-in;
         }
-        div#reconstructive-banner-container {
-          max-width: 500px;
-          height: 60px;
-          border: 2px solid #FF8B00;
+        #wrapper.closed {
+          display: none;
+        }
+        #container {
+          border: 2px solid #451212;
           background-color: #F2FFE3;
-          border-radius: 5px;
+          border-radius: 10px;
           color: #1B4869;
-          margin: 10px auto;
+          max-width: 600px;
+          margin: auto;
           padding: 5px;
+          box-shadow: 0 0 20px;
           display: grid;
-          grid-gap: 5px;
-          box-shadow: 0 0 10px;
+          grid-template-columns: 42px 20px 20px 1fr 20px 20px 20px;
+          grid-template-rows: 20px 20px 1fr;
+          grid-gap: 2px 10px;
+          box-sizing: border-box;
+          min-height: 50px;
+          transition: all 0.5s ease-in;
+        }
+        .expanded #container {
+          height: calc(100vh - 20px);
+          box-shadow: none;
+          transition: all 0.5s ease-out;
+        }
+        .fab #collapse, .fab #meta, .expanded #expand {
+          display: none;
         }
         input {
-          padding: 3px;
-          box-sizing:border-box;
+          padding: 0 5px;
+          box-sizing: border-box;
         }
-        input.urir {
-          grid-column: 1 / 3;
+        .branding {
+          width: 42px;
+        }
+        .icon {
+          width: 20px;
+        }
+        #logo {
+          grid-column: 1;
+          grid-row: 1 / 3;
+        }
+        #urir {
+          grid-column: 2 / 7;
           grid-row: 1;
         }
-        p.label {
-          grid-column: 1;
-          grid-row: 2;
-          margin: 0;
-        }
-        input.datetime {
+        #first {
           grid-column: 2;
           grid-row: 2;
         }
+        #prev {
+          grid-column: 3;
+          grid-row: 2;
+        }
+        #current {
+          grid-column: 4;
+          grid-row: 2;
+          margin: 0 5px;
+          font-size: 16px;
+          line-height: 22px;
+          color: #323B40;
+          text-align: center;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+        #next {
+          grid-column: 5;
+          grid-row: 2;
+        }
+        #last {
+          grid-column: 6;
+          grid-row: 2;
+        }
+        #close {
+          grid-column: 7;
+          grid-row: 1;
+        }
+        #expand {
+          grid-column: 7;
+          grid-row: 2;
+        }
+        #collapse {
+          grid-column: 7;
+          grid-row: 2;
+        }
+        #meta {
+          grid-column: 1 / 8;
+          grid-row: 3;
+          overflow: auto;
+          padding: 10px;
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr;
+          grid-template-rows: auto auto auto 130px 1fr;
+        }
       </style>
-      <div id="reconstructive-banner-aligner" class="">
-        <div id="reconstructive-banner-container">
-          <input class="urir" value="${this.urir}">
-          <p class="label">Archived at:</p>
-          <input class="datetime" value="${this.datetime}">
+      <div id="wrapper" class="fab">
+        <div id="container">
+          <img id="logo" class="branding" src="${this.logoSrc}" alt="Reconstructive Banner Logo">
+          <input id="urir" class="url" value="${this.urir}">
+          <a id="first" class="icon" title="${this.firstDatetime}" href="${this.firstUrim}">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M4 28v-24h4v11l10-10v10l10-10v22l-10-10v10l-10-10v11z"></path></svg>
+          </a>
+          <a id="prev" class="icon" title="${this.prevDatetime}" href="${this.prevUrim}">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M8 28v-24h4v11l10-10v22l-10-10v11z"></path></svg>
+          </a>
+          <p id="current" class="time" title="${this.mementoDatetime}">${this.timeDiff}</p>
+          <a id="next" class="icon" title="${this.nextDatetime}" href="${this.nextUrim}">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M24 4v24h-4v-11l-10 10v-22l10 10v-11z"></path></svg>
+          </a>
+          <a id="last" class="icon" title="${this.lastDatetime}" href="${this.lastUrim}">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M28 4v24h-4v-11l-10 10v-10l-10 10v-22l10 10v-10l10 10v-11z"></path></svg>
+          </a>
+          <a id="close" class="icon" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path></svg>
+          </a>
+          <a id="expand" class="icon" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M32 0h-13l5 5-6 6 3 3 6-6 5 5z"></path><path d="M32 32v-13l-5 5-6-6-3 3 6 6-5 5z"></path><path d="M0 32h13l-5-5 6-6-3-3-6 6-5-5z"></path><path d="M0 0v13l5-5 6 6 3-3-6-6 5-5z"></path></svg>
+          </a>
+          <a id="collapse" class="icon" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M18 14h13l-5-5 6-6-3-3-6 6-5-5z"></path><path d="M18 18v13l5-5 6 6 3-3-6-6 5-5z"></path><path d="M14 18h-13l5 5-6 6 3 3 6-6 5 5z"></path><path d="M14 14v-13l-5 5-6-6-3 3 6 6-5 5z"></path></svg>
+          </a>
+          <div id="meta">
+            <!-- TODO: Add provenance infomration, metadata, and interactive visualizations here... -->
+          </div>
         </div>
       </div>
     `;
     this.shadow.innerHTML = template;
 
-    let container = this.shadow.getElementById('reconstructive-banner-container');
+    const container = this.shadow.getElementById('container');
+    const wrapper = this.shadow.getElementById('wrapper');
+    const closer = this.shadow.getElementById('close');
+    const collapser = this.shadow.getElementById('collapse');
+    const expander = this.shadow.getElementById('expand');
+
     this.focused = false;
     container.onmouseover = () => this.focused = true;
     container.onmouseout = () => this.focused = false;
-    let aligner = this.shadow.getElementById('reconstructive-banner-aligner');
-    let t;
-    let resetTimer = () => {
-      aligner.classList.remove('hidden');
-      clearTimeout(t);
-      t = setTimeout(() => !this.focused && aligner.classList.add('hidden'), 2000);
+    let focusTimer;
+    const resetTimer = () => {
+      wrapper.classList.remove('hidden');
+      clearTimeout(focusTimer);
+      focusTimer = setTimeout(() => !this.focused && wrapper.classList.contains('fab') && wrapper.classList.add('hidden'), 2000);
     }
     window.addEventListener('load', resetTimer);
     window.addEventListener('mousemove', resetTimer);
@@ -78,6 +233,25 @@ class ReconstructiveBanner extends HTMLElement {
     window.addEventListener('click', resetTimer);
     window.addEventListener('scroll', resetTimer);
     window.addEventListener('keypress', resetTimer);
+
+    closer.onclick = e => {
+      e.preventDefault();
+      wrapper.classList.remove('fab', 'expanded');
+      wrapper.classList.add('closed');
+    };
+    collapser.onclick = e => {
+      e.preventDefault();
+      wrapper.classList.replace('expanded', 'fab');
+    };
+    expander.onclick = e => {
+      e.preventDefault();
+      wrapper.classList.replace('fab', 'expanded');
+    };
+    wrapper.onclick = e => {
+      if (e.target == wrapper) {
+        wrapper.classList.replace('expanded', 'fab');
+      }
+    };
   }
 }
 
